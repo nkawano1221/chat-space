c95969eb3162f59d849cd6a709465bca2129313d
Naaaaaooooo <ka.nyo1221@gmail.com>
2020-02-07 20:33:23 +0900

    グループの新規登録・編集機能

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace
@@ -13,7 +11,7 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
+    end
       config.i18n.default_locale = :ja
     end
-  end
 end
