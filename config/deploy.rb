32351e69dc012b91ca8853b338c86493ae49ff93
Naaaaaooooo <ka.nyo1221@gmail.com>
2020-02-15 19:18:25 +0900

    added depol.erb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,8 @@ set :repo_url,  'git@github.com:Naaaaaooooo/chat-space.git'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+set :linked_files, %w{ config/secrets.yml }
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
@@ -23,13 +25,6 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 # Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
 
 # デプロイ処理が終わった後、unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
